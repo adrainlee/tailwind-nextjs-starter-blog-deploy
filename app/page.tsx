ec9fb8a92f21bcec9c66014b072df3334263cd5f
adrainlee <adrainlee8@gmail.com>
2025-02-20 13:14:27 +0000

    test: trigger build with local binary configurations

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
-// Test CICD with npm configuration - using local tsc binary
+// Test CICD with npm configuration - using local binaries
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
