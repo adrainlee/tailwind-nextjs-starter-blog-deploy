f76eee4a2fa4a000bf1282a3d3fe55bcecc861ec
adrainlee <adrainlee8@gmail.com>
2025-02-20 12:25:47 +0000

    test: trigger CICD with npm configuration

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
+// Test CICD with npm configuration
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
